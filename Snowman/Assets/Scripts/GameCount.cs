b0120a05d7c3f53d167c9ba3c5405d724802db7b
Israel Anthony (RIT Student) <ixa4371@ad.rit.edu>
2016-09-09 11:55:50 -0400

    Finished the scene

---
+++ Snowman/Assets/Scripts/GameCount.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCount : MonoBehaviour 
+{
+	public int count; // number of destroyed snowmen
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log ("GameCount added");
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnGUI()
+	{
+		GUI.Box (new Rect (0, 0, 150, 20), "Destroyed Snowmen: " + count.ToString ());
+	}
+}
---
+++ Snowman/Bin/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Test : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log ("GameManager created");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Debug.Log ("Running!");
+	}
+}
